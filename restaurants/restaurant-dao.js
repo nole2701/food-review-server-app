005092cf39c6b08223d401f704f21960281bfa2e
nole2701 <nole2701@gmail.com>
2023-04-23 03:49:12 -0400

    added semicolon

--- restaurants/restaurant-dao.js
+++ restaurants/restaurant-dao.js
@@ -3,5 +3,5 @@ export const findRestaurants = () => restaurantModel.find();
 export const findRestaurantById = (resId) => restaurantModel.findById({_id: resId});
 export const findRestaurantByName = (resName) => restaurantModel.find({name: resName});
 export const createRestaurant = (restaurant) => restaurantModel.create(restaurant);
-export const updateRestaurant = (resId, restaurant) => restaurantModel.updateOne({_id: resId}, {$set: restaurant})
+export const updateRestaurant = (resId, restaurant) => restaurantModel.updateOne({_id: resId}, {$set: restaurant});
 export const deleteRestaurant = (resId) => restaurantModel.deleteOne({_id: resId});

c155fbbc397cfbb407f1d1c71e3e5f47f3748265
bkornusov <batyrkornusov@gmail.com>
2023-04-27 09:26:03 -0400

    findById working

--- restaurants/restaurant-controller.js
+++ restaurants/restaurant-controller.js
@@ -1,50 +1,48 @@
-import * as restaurantDao from './restaurant-dao.js';
+import * as restaurantDao from "./restaurant-dao.js";
 
 export default (app) => {
-    app.get('/api/restaurants', findRestaurants);
-    app.get('/api/restaurants/:resName', findRestaurantByName);
-    app.get('/api/restaurants/:rid', findRestaurantById);
-    app.post('/api/restaurants', createRestaurant);
-    app.put('/api/restaurants/:rid', updateRestaurant);
-    app.delete('/api/restaurants/:rid', deleteRestaurant);
-}
-    
+  app.get("/api/restaurants", findRestaurants);
+  app.get("/api/restaurants/:resName", findRestaurantByName);
+  app.get("/api/restaurants/id/:rid", findRestaurantById);
+  app.post("/api/restaurants", createRestaurant);
+  app.put("/api/restaurants/:rid", updateRestaurant);
+  app.delete("/api/restaurants/:rid", deleteRestaurant);
+};
 
 const findRestaurants = async (req, res) => {
-    const restaurants = await restaurantDao.findRestaurants();
-    res.json(restaurants);
-}
+  const restaurants = await restaurantDao.findRestaurants();
+  res.json(restaurants);
+};
 
 const findRestaurantById = async (req, res) => {
-    const restaurantId = req.params['rid'];
-    const restaurant = await restaurantDao.findRestaurantById(restaurantId);
-    res.json(restaurant);
-}
+  const restaurantId = req.params["rid"];
+  const restaurant = await restaurantDao
+    .findRestaurantById(restaurantId)
+    .exec();
+  res.json(restaurant);
+};
 
 const findRestaurantByName = async (req, res) => {
-    const restaurantName = req.params['resName'];
-    const restaurants = await restaurantDao.findRestaurantByName(restaurantName);
-    res.json(restaurants);
-}
+  const restaurantName = req.params["resName"];
+  const restaurants = await restaurantDao.findRestaurantByName(restaurantName);
+  res.json(restaurants);
+};
 
 const createRestaurant = async (req, res) => {
-    const restaurant = req.body;
-    const status = await restaurantDao.createRestaurant(restaurant);
-    res.json(status);
-}
+  const restaurant = req.body;
+  const status = await restaurantDao.createRestaurant(restaurant);
+  res.json(status);
+};
 
 const updateRestaurant = async (req, res) => {
-    const restaurantId = req.params['rid'];
-    const updates = req.body;
-    const status = await restaurantDao.updateRestaurant(restaurantId, updates);
-    res.json(status);
-}
+  const restaurantId = req.params["rid"];
+  const updates = req.body;
+  const status = await restaurantDao.updateRestaurant(restaurantId, updates);
+  res.json(status);
+};
 
 const deleteRestaurant = async (req, res) => {
-    const restaurantId = req.params['rid'];
-    const status = await restaurantDao.deleteRestaurant(restaurantId);
-    res.json(status);
-}
-
-    
-
+  const restaurantId = req.params["rid"];
+  const status = await restaurantDao.deleteRestaurant(restaurantId);
+  res.json(status);
+};
--- restaurants/restaurant-dao.js
+++ restaurants/restaurant-dao.js
@@ -1,7 +1,12 @@
 import restaurantModel from "./restaurant-model.js";
 export const findRestaurants = () => restaurantModel.find();
-export const findRestaurantById = (resId) => restaurantModel.findById({_id: resId});
-export const findRestaurantByName = (resName) => restaurantModel.find({name: resName});
-export const createRestaurant = (restaurant) => restaurantModel.create(restaurant);
-export const updateRestaurant = (resId, restaurant) => restaurantModel.updateOne({_id: resId}, {$set: restaurant});
-export const deleteRestaurant = (resId) => restaurantModel.deleteOne({_id: resId});
+export const findRestaurantById = (resId) =>
+  restaurantModel.findOne({ _id: resId });
+export const findRestaurantByName = (resName) =>
+  restaurantModel.find({ name: resName });
+export const createRestaurant = (restaurant) =>
+  restaurantModel.create(restaurant);
+export const updateRestaurant = (resId, restaurant) =>
+  restaurantModel.updateOne({ _id: resId }, { $set: restaurant });
+export const deleteRestaurant = (resId) =>
+  restaurantModel.deleteOne({ _id: resId });
